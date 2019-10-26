12a86ee2bf4de866a25f4ad5061cab39ab4e2a72
Vasg <undacvasg@gmail.com>
2019-10-26 00:32:10 -0500

    produccion

--- frontend/services/BookService.js
+++ frontend/services/BookService.js
@@ -1,6 +1,6 @@
 class BookService{
     constructor(){
-        this.URI='http://localhost:4000/api/books';        
+        this.URI='/api/books';        
     }
     async getBooks(){
         const response=await fetch(this.URI);
